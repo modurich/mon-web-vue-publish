d137c44cac65afc93db118f3895d9c53c72bc5a4
kunsuk <kunsuk@gmail.com>
2022-09-30 23:01:01 +0900

    test

--- src/pages/publish/Index.vue
+++ src/pages/publish/Index.vue
@@ -1,9 +1,20 @@
 <template>
-    <div>publish list</div>
+    <div>
+        <div>publish list</div>
+        <m-checkbox title="테스트" />
+    </div>
 </template>
 
 <script>
+import { ref } from 'vue';
+import MCheckbox from 'src/components/MCheckbox.vue';
 export default {
-    name: 'publish'
+    components: { MCheckbox },
+    name: 'publish',
+    setup() {
+        return {
+            val: ref(true)
+        };
+    }
 };
 </script>
