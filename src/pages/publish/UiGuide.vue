6a4e72914dfcb253daaee8f8905c000834c2642f
kunsuk <kunsuk@gmail.com>
2022-10-01 00:44:25 +0900

    dd

--- src/pages/publish/UiGuide.vue
+++ src/pages/publish/UiGuide.vue
@@ -1,9 +1,43 @@
 <template>
-    <div>uiguide</div>
+    <div>
+        <m-tabs :items="[
+            {'label':'Tab1', 'value':'Tab1', 'to':'#aa'},
+            {'label':'Tab2', 'value':'Tab2', 'to':'#bb'}]"
+        />
+        <!--
+        <m-alert :content="내용" :button-label="확인">
+        </m-alert>
+
+        <m-header />
+
+        <m-dialog />
+        -->
+        <div>
+            <h1>메뉴</h1> 
+        <m-menu :items="[
+            {'label':'Menu1', 'value':'Menu1', 'to':'#aa'},
+            {'label':'Menu2', 'value':'Menu2', 'to':'#bb'}]"/>
+
+        </div>
+        <div>
+            <h1>버튼</h1>
+        <m-button-3 >
+            버튼테스트
+        </m-button-3>
+        </div>
+        <m-input />
+    </div>
 </template>
 
 <script>
+import MTabs from 'src/components/MTabs.vue';
+import MAlert from 'src/components/MAlert.vue';
+import MHeader from 'src/components/MHeader.vue';
+import MMenu from 'src/components/MMenu.vue';
+import MButton3 from 'src/components/MButton3.vue';
+import MInput from 'src/components/MInput.vue';
 export default {
-    name: 'publish'
+    components: { MTabs, MAlert, MHeader, MMenu, MButton3 },
+        MInputname: 'publish'
 };
 </script>
