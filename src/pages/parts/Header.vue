5a16d9f4e51a9e68aeae1d26f4aa3c7bb9992e7a
eunhye.choi <trick1120.hanmail.net>
2022-10-02 16:49:38 +0900

    수정

--- src/pages/parts/Header.vue
+++ src/pages/parts/Header.vue
@@ -17,8 +17,13 @@
                     </ul>
                 </nav>
             </div>
-            <div class="search_wrap">
-                <button class="ico_search">seach</button>
+            <div class="btn_wrap">
+                <button class="ico_search ">seach</button>
+                <!-- <button class="ico_luckybox mob_only">luckybox</button>
+                <button class="ico_alarm01 mob_only">alarm</button>
+                <div class="user_img mob_only">
+                    <div class="img_area"><img src="~assets/user_img.png" alt="profile"/></div>
+                </div> -->
                 <m-button-3  color="slateBlue" size="small" class="mob_only">로그인</m-button-3>
             </div>
         </div>
@@ -42,14 +47,14 @@ export default {
         border-bottom:1px solid #d4d9ea;
         margin-bottom:20px;
         
-       .mob_only{display:block}
+        .mob_only{display:none}
         .header_area{
             height:80px;
             width:1284px;margin:0 auto;
             display:flex;
             align-items: center;
             justify-content: space-between;
-            .search_wrap{
+            .btn_wrap{
                 display: inline-flex;
                 align-items: center;
                 .ico_search{
@@ -119,12 +124,60 @@ export default {
         }
         //1284보다 작아질때
         @media (max-width: 1284px) {
-            border:1px solid red;
-            .header_area{width:100%}
+            padding:0 16px;
+            height:60px;
+            .header_area{width:100%;
+                height: 60px;
+                .btn_wrap{
+                    display: inline-flex;
+                    align-items: center;
+                    button + button{margin-left:20px}
+                    .user_img{
+                        width:24px;
+                        height:24px;
+                        border-radius:24px;
+                        margin-left:20px;
+                        overflow: hidden;
+                        img{width:100%;height:100%}
+                    }
+                    .ico_search{
+                        background-size:contain !important;
+                        width:24px !important;
+                        height: 24px !important;
+                    }
+                    .m-button3{margin-left:30px}
+                    .ico_luckybox{
+                        background: url('~assets/ico_luckybox.svg') center right no-repeat;
+                        display: inline-flex;
+                        width:24px;
+                        height: 24px;
+                        text-indent: -999em;
+                    }
+                    .ico_alarm01{
+                        background: url('~assets/ico_alarm01.svg') center right no-repeat;
+                        display: inline-flex;
+                        width:24px;
+                        height: 24px;
+                        text-indent: -999em;
+                    }
+                }
+            }
             .mob_only{
                 display: block;
             }
             nav{display: none !important;}
+            .nav_wrap{
+                .m_logo{
+                    display: inline-flex;
+                    width:30px !important;
+                    height: 30px;
+                    margin-right:10px;
+                    text-indent: -999em;
+                    background: url('~assets/logo.svg') center right no-repeat !important;;
+                    background-size:contain !important;
+                }
+            }
+            
         }
     }
 </style>
