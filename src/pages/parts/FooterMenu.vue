49af1229a198fec8d99a855f80cf523cd4fcae9d
kunsuk <kunsuk@gmail.com>
2022-10-04 21:52:50 +0900

    footer menu

--- src/pages/parts/FooterMenu.vue
+++ src/pages/parts/FooterMenu.vue
@@ -3,11 +3,19 @@
     <div class="footer_menu">
         <nav>
             <ul class="nav_area">
+                <li :key="`m-menu-item-${idx}`" v-for="(item, idx) in menuList" @click="handleClick(item)">
+                    <a :class="`${item.val == menu ? 'active' : 'inactive'}`" href="#">
+                        <i :class="`ico top_${item.val}`" />
+                        <span class="txt">{{item.label}}</span>
+                    </a>
+                </li>
+                <!--
                 <li><a href="#" class="active"><i class="ico top_menu01"/></a></li>
                 <li><a href="#"><i class="ico top_menu02"/></a></li>
                 <li><a href="#"><i class="ico top_menu03"/></a></li>
                 <li><a href="#"><i class="ico top_menu04"/></a></li>
                 <li><a href="#"><i class="ico top_menu05"/></a></li>
+                -->
             </ul>
         </nav>
     </div>
@@ -16,13 +24,36 @@
 <script>
 export default {
     name: 'Footer',
-    components: {
-    },
     data() {
         return {
-            
+            menu: "menu01",
+            menuList: [
+                { label: "홈", val: "menu01", to: "" },
+                { label: "랭킹", val: "menu02", to: "" },
+                { label: "콘텐츠", val: "menu03", to: "" },
+                { label: "거버넌스", val: "menu04", to: "" },
+                { label: "", val: "menu05", to: "" },
+            ]
         };
     },
+    methods: {
+        handleClick(item) {
+            // console.log('menu item clicked!!!!', item.val);
+            // console.log(this.menu);
+            this.menu = item.val;
+            if (!item.to) this.$emit('input', item.val);
+            if (item.onClick) {
+                this.$nextTick(() => item.onClick(item));
+            }
+        }
+    },
+    created() {
+    },
+    updated() {
+
+    },
+    mounted() {
+    }
 };
 </script>
 
