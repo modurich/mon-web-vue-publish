59028f30fcedd41a0850050b63b4ea62a5931d92
kunsuk <kunsuk@gmail.com>
2022-10-04 21:30:32 +0900

    slide

--- src/pages/parts/Raside.vue
+++ src/pages/parts/Raside.vue
@@ -55,56 +55,56 @@
                 <q-carousel-slide name="2" class="column no-wrap flex-center">
                     <ol class="ranking_list">
                         <li>
-                            <span class="txt01">1</span>
+                            <span class="txt01">6</span>
                             <span class="img"><img src="~assets/ranking_pro1.png" alt="프로필사진" /></span>
                             <span class="txt02">몬스탁닉네임</span>
                         </li>
                         <li>
-                            <span class="txt01">2</span>
+                            <span class="txt01">7</span>
                             <span class="img"><img src="~assets/ranking_pro2.png" alt="프로필사진" /></span>
                             <span class="txt02">호빵맨</span>
                         </li>
                         <li>
-                            <span class="txt01">3</span>
+                            <span class="txt01">8</span>
                             <span class="img"><img src="~assets/ranking_pro3.png" alt="프로필사진" /></span>
                             <span class="txt02">식빵맨</span>
                         </li>
                         <li>
-                            <span class="txt01">4</span>
+                            <span class="txt01">9</span>
                             <span class="img"><img src="~assets/ranking_pro4.png" alt="프로필사진" /></span>
                             <span class="txt02">쨜랑이</span>
                         </li>
                         <li>
-                            <span class="txt01">5</span>
+                            <span class="txt01">10</span>
                             <span class="img"><img src="~assets/ranking_pro5.png" alt="프로필사진" /></span>
                             <span class="txt02">세균맨</span>
                         </li>
                     </ol>
                 </q-carousel-slide>
-                <q-carousel-slide name="2" class="column no-wrap flex-center">
+                <q-carousel-slide name="3" class="column no-wrap flex-center">
                     <ol class="ranking_list">
                         <li>
-                            <span class="txt01">1</span>
+                            <span class="txt01">11</span>
                             <span class="img"><img src="~assets/ranking_pro1.png" alt="프로필사진" /></span>
                             <span class="txt02">몬스탁닉네임</span>
                         </li>
                         <li>
-                            <span class="txt01">2</span>
+                            <span class="txt01">12</span>
                             <span class="img"><img src="~assets/ranking_pro2.png" alt="프로필사진" /></span>
                             <span class="txt02">호빵맨</span>
                         </li>
                         <li>
-                            <span class="txt01">3</span>
+                            <span class="txt01">13</span>
                             <span class="img"><img src="~assets/ranking_pro3.png" alt="프로필사진" /></span>
                             <span class="txt02">식빵맨</span>
                         </li>
                         <li>
-                            <span class="txt01">4</span>
+                            <span class="txt01">14</span>
                             <span class="img"><img src="~assets/ranking_pro4.png" alt="프로필사진" /></span>
                             <span class="txt02">쨜랑이</span>
                         </li>
                         <li>
-                            <span class="txt01">5</span>
+                            <span class="txt01">15</span>
                             <span class="img"><img src="~assets/ranking_pro5.png" alt="프로필사진" /></span>
                             <span class="txt02">세균맨</span>
                         </li>
