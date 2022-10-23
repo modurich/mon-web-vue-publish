685d0c11d5889e1de82ba0a134e459cc1a8ca2b6
trick1120 <trick1120@hanmail.net>
2022-10-23 15:00:25 +0900

    footer_label

--- src/pages/parts/FooterMenu.vue
+++ src/pages/parts/FooterMenu.vue
@@ -5,7 +5,7 @@
             <ul class="nav_area">
                 <li :key="`m-menu-item-${idx}`" v-for="(item, idx) in menuList" @click="handleClick(item)">
                     <a :class="`${item.val == menu ? 'active' : 'inactive'}`" href="#">
-                        <i :class="`ico top_${item.val}`" />
+                        <i :class="`ico top_${item.val}`" />{{item.label}}
                     </a>
                 </li>
                 <!--
