4c2b10271b7cf5d6fa0f727d162ec51da790a63a
eunhye.choi <trick1120.hanmail.net>
2022-11-04 21:51:01 +0900

    수정

--- src/pages/publish/H_11112_U.vue
+++ src/pages/publish/H_11112_U.vue
@@ -13,10 +13,32 @@
             </div>
             <q-card-section class="dg_con">
               <p class="dialog_tit">자산 순서 변경</p>
-             
+              <draggable v-model="items">
+                <transition-group>
+                  <div class="product_list" v-for="item in items" :key="item">
+                    <div>
+                      <div class="prd_img">
+                        <div class="img_area">
+                          <img src="~assets/prd_logo.png" alt="profile" />
+                        </div>
+                        <div>
+                          <span class="font16 txt_dk">{{item.name}}</span>
+                          <span class="txt02">
+                            {{item.number}}
+                          </span>
+                        </div>
+                      </div>
+                      <div class="ico_area">
+                        <i class="ico_menu01" />
+                      </div> 
+                    </div>
+                  </div>
+                  </transition-group>
+              </draggable>
+              <!-- S 전체-->
             </q-card-section>
             <q-card-actions class="fixed_btn_wrap">
-              <m-button-3 flat size="large" color="primary" v-close-popup class="full">확인</m-button-3>
+              <m-button-3 flat size="large" color="primary" v-close-popup class="full">저장하기</m-button-3>
             </q-card-actions>
           </div>
         </m-dialog>
@@ -27,15 +49,24 @@
 </template>
 
 <script>
+import draggable from 'vuedraggable'
 import { ref } from 'vue';
 import MDialogBlank from 'src/components/MDialogBlank.vue';
 
 export default {
-  components: { MDialogBlank },
+  components: { MDialogBlank,draggable },
   name: 'A_10000_P',
   data() {
     return {
       dailog2: ref(false),
+      items:[
+        {name:'삼성전자우',number:'코스피 005930'},
+        {name:'삼성전자우1',number:'코스피 005930'},
+        {name:'삼성전자우2',number:'코스피 005930'},
+        {name:'삼성전자우3',number:'코스피 005930'},
+        {name:'삼성전자우4',number:'코스피 005930'},
+        {name:'삼성전자우5',number:'코스피 005930'},
+      ]
     }
   },
   methods: {
