6a4e2ed87dafde56cfece285fcf67b6e034dbc73
kunsuk <kunsuk@gmail.com>
2022-11-10 22:14:41 +0900

    a

--- src/components/GaugeChart.vue
+++ src/components/GaugeChart.vue
@@ -12,98 +12,97 @@ HighchartsMore(Highcharts);
 SolidGuage(Highcharts);
 
 export default {
-  components: { 
-    
-  },
-    name: 'GaugeChart',
-    props: {
-        rating: {
-            type: Object
-        }
-    },
-    data() {
-        return {
-        };
-    },
-    computed: {
-        chartOptions() {
-            return {
-              chart: {
-                type: 'solidgauge',
-                height: 210,
-                width: 210
-              },
-
-              title: {
-                text:''
-              },
-              credits: {
-                enabled: false
-              },
-              tooltip: {
-                enabled: false
-              },      
-              pane: {
-                startAngle:-125,
-                endAngle: 125,
-                background: [{ // Track for Move
-                  outerRadius: '114%',
-                  innerRadius: '88%',
-                  backgroundColor: Highcharts.color(Highcharts.getOptions().colors[0]).setOpacity(0.0).get(),
-                  borderWidth: 0,
-                }]
-              },
-
-              yAxis: {
-                min: 0,
-                max: 100,
-                lineWidth: 0,
-                tickPositions: []
-              },
-
-              plotOptions: {
-                solidgauge: {
-                  dataLabels: {
-                    enabled: true
-                  },
-                  linecap: 'round',
-                  stickyTracking: false,
-                  rounded: true
-                }
-              },
-
-              series: [{
-                name: '현재수익률',
-                data: [{
-                  color: Highcharts.getOptions().colors[0],
-                  radius: '114%',
-                  innerRadius: '88%',
-                  y: 60
-                }, {
-                  color: Highcharts.color(Highcharts.getOptions().colors[0]).setOpacity(0.3).get(),
-                  radius: '114%',
-                  innerRadius: '88%',
-                  y: 100
-                  }
-                ],
-                dataLabels: {
-                  format: '<div class="label_wrap"><span class="txt01">현재수익률</span><span class="txt02" >0000.0%</span></div> ',
-                  borderWidth: 0,
-                  useHTML: true
-                },
-              }]
-            };
-          }
-    },
-    beforeCreate() {
-    },
-    async created() {
-    },
-    mounted() {
-
-    },
-    methods: {
+  components: { },
+  name: 'GaugeChart',
+  props: {
+    rating: {
+      type: Object
     }
+  },
+  data() {
+    return {
+    };
+  },
+  computed: {
+    chartOptions() {
+      return {
+        chart: {
+          type: 'solidgauge',
+          height: 210,
+          width: 210
+        },
+
+        title: {
+          text: ''
+        },
+        colors: ['#43c890', 'orange'],
+        credits: {
+          enabled: false
+        },
+        tooltip: {
+          enabled: false
+        },
+        pane: {
+          startAngle: -125,
+          endAngle: 125,
+          background: [{ // Track for Move
+            outerRadius: '114%',
+            innerRadius: '88%',
+            backgroundColor: Highcharts.color(Highcharts.getOptions().colors[0]).setOpacity(0.0).get(),
+            borderWidth: 0,
+          }]
+        },
+
+        yAxis: {
+          min: 0,
+          max: 100,
+          lineWidth: 0,
+          tickPositions: []
+        },
+
+        plotOptions: {
+          solidgauge: {
+            dataLabels: {
+              enabled: true
+            },
+            linecap: 'round',
+            stickyTracking: false,
+            rounded: true
+          }
+        },
+
+        series: [{
+          name: '현재수익률',
+          data: [{
+            color: Highcharts.color('#43c890').setOpacity(1).get(),
+            radius: '114%',
+            innerRadius: '88%',
+            y: 60
+          }, {
+            color: Highcharts.color('#43c890').setOpacity(0.3).get(),
+            radius: '114%',
+            innerRadius: '88%',
+            y: 100
+          }
+          ],
+          dataLabels: {
+            format: '<div class="label_wrap"><span class="txt01">현재수익률</span><span class="txt02">0000.0%</span></div> ',
+            borderWidth: 0,
+            useHTML: true
+          },
+        }]
+      };
+    }
+  },
+  beforeCreate() {
+  },
+  async created() {
+  },
+  mounted() {
+
+  },
+  methods: {
+  }
 };
 </script>
 
